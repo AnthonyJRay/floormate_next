f2e104654deeb33b18de1ab7eab12c8cbd8dad14
AnthonyJRay <ajrayid@gmail.com>
2023-01-15 13:29:27 -0600

    Tweak Estimate Display so cards display more evenly.

--- src/features/estimates/estimate-display/index.jsx
+++ src/features/estimates/estimate-display/index.jsx
@@ -5,7 +5,7 @@ import { IconButton, TextButton } from "@/ui/button";
 import Card from "@/ui/card";
 
 export default function EstimateDisplay({ data, className, onDelete }) {
-  const defaultClassName = "flex flex-col grow basis-0 gap-1 m-1 bg-gray-800";
+  const defaultClassName = "flex flex-col grow gap-1 m-1 bg-gray-800";
   const { estimateNO, estimateDate, client, summary, total, invoiced } = data;
   return (
     <Card className={`${defaultClassName} ${className}`}>
@@ -17,13 +17,12 @@ export default function EstimateDisplay({ data, className, onDelete }) {
         {client.firstName} {client.lastName}
       </div>
       <div className="my-4">{summary}</div>
+
       <div className="text-green-500">${total}</div>
       <div className={invoiced ? "text-green-500" : "text-red-500"}>
         {invoiced ? "Invoiced" : "Not Invoiced"}
       </div>
       <div className="flex justify-center gap-2 my-2">
-        {/* How to carry over props through View button 
-        to populate Form state instead of empty defaults?" */}
         <Link href="/estimates/new-estimate">
           <div>
             <TextButton className="bg-yellow-500 hover:bg-yellow-400">
