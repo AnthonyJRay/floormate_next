a411535eba919f6abd4fdbad193a87bcad691de1
AnthonyJRay <ajrayid@gmail.com>
2023-02-04 05:27:58 -0600

    Added a message on Homepage intended for new users to get started by creating their first Estimate or Invoice.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,20 +1,30 @@
 import { getSession } from "next-auth/react";
 import { getUser } from "@/api/user";
 
+import Link from "next/Link";
+
 export default function Homepage({}) {
   return (
     <div className={"w-full m-2 text-center text-gray-700"}>
       <h1 className={"text-center"}>Dashboard</h1>
+      <h3>
+        To get started, create your first Estimate and Invoice it,
+        {<Link href="/estimates/new-estimate">here!</Link>}
+      </h3>
     </div>
   );
 }
 
 export async function getServerSideProps(context) {
   const sessionData = await getSession(context);
-
   if (sessionData) {
     const userData = await getUser(sessionData);
-    console.log("User Data coming from API/USER", userData);
+    console.log("user data from mongo", userData);
+    return {
+      props: {
+        userID: JSON.parse(JSON.stringify(userData._id)),
+      },
+    };
   }
   return {
     props: {
